1b3a5dfdabb59a1d0b218375c0020cf03734ef0f
Liubov Klimova <luibovklimova@gmail.com>
2019-02-03 23:37:49 -0500

    reorganized annotations

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -14,11 +16,16 @@ import java.util.concurrent.TimeUnit;
 public class LoginTests {
     WebDriver driver;
 
-    @BeforeClass
+    @BeforeMethod
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void cleanUp(){
+        driver.close();
     }
 
     @Test
